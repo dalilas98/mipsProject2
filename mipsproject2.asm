576ea39ddd89a1721ee67215c7da2bf1a0c21d00
dalilas98 <44147704+dalilas98@users.noreply.github.com>
2018-11-11 20:20:43 -0500

    Add files via upload
    trying to implement if statements to print "input is too short"

--- mipsproject2.asm
+++ mipsproject2.asm
@@ -1,6 +1,8 @@
 .data
  str: 
  	.space 16
+ str1:  
+ 	.asciiz "Input is too short" 
 .text
  main: 
   	li $v0, 8 
@@ -12,4 +14,7 @@
   	la $a0, str
   	syscall 
   
- 
+	slt $t1, $t2, $t3
+	beq $t1, -100, label 
+	li $str1
+	syscall 
