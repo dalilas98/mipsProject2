caf8722431d1cf8f2f135c3cab82151e7b7b72e6
dalilas98 <44147704+dalilas98@users.noreply.github.com>
2018-11-16 12:31:43 -0500

    Add files via upload
    encountered a problem with user input. I prints out a zero instead of printing out correct message

--- project2.s
+++ project2.s
@@ -9,13 +9,13 @@ storage: .space 4
 main: 
 	la $a0, userInput
 	li $v0, 8 
+	li $a1, 2000
 	syscall 
 	
 	li $t0, 0 #updated for non 0 numbers 
 	li $s0, 1 
 	li $t3, 3 
 	li $s1, 27 # my base number 
-	
 	
 	loop: 
 	lb $t1, 0($a0)
@@ -25,37 +25,44 @@ main:
 	beq $t1,' ', loop 
 	
 	blt $t1, 48, printInvalidNum
-	bgt $t1, 118, printInvalidNum
-	
-	
-	
+	bgt $t1, 118, exit
+	#beqz $t0, exit 
 	
 	j loop
 	
 	exit:
 	
 	
+	li $v0, 1 
+	bgt $t0, 4, printTooLong
+	move $a0, $t0
+	syscall 
+	j end 
+	
+	
+	
 
+	#after reading  thru string 	
+   	 printEmpty:
+    	la $a0, emptyInput                  
+    	li $v0, 4                                   
+    	syscall
+    	j end
 
-    printEmpty:
-    la $a0, emptyInput                  
-    li $v0, 4                                   
-    syscall
-    jal exit
+    	printInvalidNum:
+    	la $a0, invalidNum                    
+    	li $v0, 4                                   
+    	syscall
+    	j end
 
-    printInvalidNum:
-    la $a0, invalidNum                    
-    li $v0, 4                                   
-    syscall
-    jal exit
+    	printTooLong:
+    	la $a0, inputTooLong                  
+    	li $v0, 4                                   # load code to print string
+    	syscall 
+    	j end 
 
-    printTooLong:
-    la $a0, inputTooLong                  
-    li $v0, 4                                   # load code to print string
-    syscall
-
-    exit:
-    li $v0, 10                                  # load code to exit the program
-    syscall
+    	end:
+    	li $v0, 10                                  # load code to exit the program
+    	syscall
 	
 	
